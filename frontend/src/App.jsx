19824512408f4ffd335297427eeefc897fae8608
Wesley-waka <wesleywaka77@gmail.com>
2023-12-28 10:43:27 +0300

    Updating app with Navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
 import './App.css'
 import { Switch, Route,Router,BrowserRouter } from 'react-router-dom';
 import AboutUs from './components/AboutUs/AboutUs';
+import NavBar from './components/Navbar/NavBar';
 
 
 function App() {
 
   return (
     <div className='App'>
+      <NavBar
+        loggedIn={loggedIn}
+        setLoggedIn={setLoggedIn}
+        userType={userType}
+        setUserType={setUserType}
+        // userPatient={userPatient}
+        // userPractitioner={userPractitioner}
+        // setUserPatient={setUserPatient}
+        // setUserPractitioner={setUserPractitioner}
+        cartItems={cartItems}
+      />
       <Switch>
       <Route exact path="/about">
           <AboutUs />
