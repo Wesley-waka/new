15536e90be22cc57afc8b7f7fcf65f3d67e069e5
Wesley_waka <wesleywaka77@gmail.com>
2023-12-29 17:12:00 +0300

    Create EditProduct Form

---
+++ frontend/src/components/Admin/EditProduct.jsx
@@ -0,0 +1,105 @@
+import AdminSidebar from '../AdminSidebar/AdminSidebar';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {  useNavigate, useParams } from "react-router-dom";
+import './EditPractitioner.css';
+import {URL as url} from '../../constants';
+
+const EditPractitioner = () => {
+  let history = useNavigate();
+  const { id } = useParams();
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+    phone: "",
+    website: ""
+  });
+  
+  const { name, username, email, phone, website } = user;
+  const onInputChange = e => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+  
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    await axios.put(`${url}/users/${id}`, user);
+    history("/admin");
+  };
+
+  const loadUser = async () => {
+    const result = await axios.get(`/users/${id}`, user);
+    setUser(result.data);
+  };
+  
+  return (
+    <div className="all-practitioners-main-container">
+        <AdminSidebar/>
+      <div className='edit-practitioner-container'>
+        <h1>Edit Practitioner</h1>
+        <section className='th'>
+        <form onSubmit={e => onSubmit(e)}>
+          <div>
+            <br/>
+            <input
+              type="text"
+              placeholder="Enter Practitioner Name"
+              name="name"
+              value={name}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div>
+            <br/>
+            <input
+              type="text"
+              placeholder="Enter Practitioner UserName"
+              name="username"
+              value={username}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div>
+            <br/>
+            <input
+              type="email"
+              placeholder="Enter Practitioner Email"
+              name="email"
+              value={email}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div>
+            <br/>
+            <input
+              type="text"
+              placeholder="Enter the Practitioner Phone Number"
+              name="phone"
+              value={phone}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <div>
+             <br/>
+            <input
+              type="text"
+              placeholder="Enter the Practitioner Website Name"
+              name="website"
+              value={website}
+              onChange={e => onInputChange(e)}
+            />
+          </div>
+          <br/>
+          <button>Update A practitioner</button>
+        </form>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default EditPractitioner;
